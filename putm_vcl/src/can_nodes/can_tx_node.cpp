f16e1e7f205f009b2664f651b028effcea5f6f7f
Adam <Adam_Wasilewski_99@proton.me>
2024-06-09 21:13:35 +0200

    Add amk data, status and rtd callback methods

--- putm_vcl/src/can_nodes/can_tx_node.cpp
+++ putm_vcl/src/can_nodes/can_tx_node.cpp
@@ -12,9 +12,12 @@ CanTxNode::CanTxNode()
       can_tx_amk(can_interface_amk),
       can_tx_common(can_interface_common),
       can_tx_amk_timer(this->create_wall_timer(10ms, std::bind(&CanTxNode::can_tx_amk_callback, this))),
-      can_tx_common_timer(this->create_wall_timer(10ms, std::bind(&CanTxNode::can_tx_common_callback, this))),
+      can_tx_common_timer(this->create_wall_timer(25ms, std::bind(&CanTxNode::can_tx_common_callback, this))),
       amk_control_subscription(this->create_subscription<msg::AmkControl>("putm_vcl/amk_control", 1, std::bind(&CanTxNode::amk_control_callback, this, _1))),
-      rtd_subscription(this->create_subscription<msg::Rtd>("putm_vcl/rtd", 1, std::bind(&CanTxNode::rtd_callback, this, _1))) {}
+      rtd_subscription(this->create_subscription<msg::Rtd>("putm_vcl/rtd", 1, std::bind(&CanTxNode::rtd_callback, this, _1))) ,
+      amk_data_subscription(this->create_subscription<msg::AmkData>("putm_vcl/amk_data", 1, std::bind(&CanTxNode::amk_data_callback, this, _1))),
+      amk_status_subscription(this->create_subscription<msg::AmkStatus>("putm_vcl/amk_status", 1, std::bind(&CanTxNode::amk_status_callback, this, _1))) {}
+      
 
 
 void CanTxNode::amk_control_callback(const msg::AmkControl msg) {
@@ -49,6 +52,36 @@ void CanTxNode::amk_control_callback(const msg::AmkControl msg) {
   amk_rear_right_setpoints.AMK_TargetVelocity = msg.amk_target_torque[Inverters::REAR_RIGHT];
   amk_rear_right_setpoints.AMK_TorqueLimitPositiv = msg.amk_torque_positive_limit[Inverters::REAR_RIGHT];
   amk_rear_right_setpoints.AMK_TorqueLimitNegativ = msg.amk_torque_negative_limit[Inverters::REAR_RIGHT];
+}
+
+void CanTxNode::amk_data_callback(const putm_vcl_interfaces::msg::AmkData msg)
+{
+  uint32_t meanRph = ((msg.amk_actual_velocity[0]) * 60);
+  float speed = (meanRph * 2 * 3.14 * 0.000205 / 14.25);
+  pcMainData.vehicleSpeed = static_cast<uint8_t>(speed);
+
+  pcMainData.frontLeftInverterTemperature = msg.amk_temp_inverter[Inverters::FRONT_LEFT];
+  pcMainData.frontRightInverterTemperature = msg.amk_temp_inverter[Inverters::FRONT_RIGHT];
+  pcMainData.rearLeftIverterTemperature = msg.amk_temp_inverter[Inverters::REAR_LEFT];
+  pcMainData.rearRightIverterTemperature = msg.amk_temp_inverter[Inverters::REAR_RIGHT];
+
+  pcMainData.frontLeftMotorTemperature = msg.amk_temp_motor[Inverters::FRONT_LEFT];
+  pcMainData.frontRightMotorTemperature = msg.amk_temp_motor[Inverters::FRONT_RIGHT];
+  pcMainData.rearLeftMotorTemperature = msg.amk_temp_motor[Inverters::REAR_LEFT];
+  pcMainData.rearRightMotorTemperature = msg.amk_temp_motor[Inverters::REAR_RIGHT];
+}
+
+void CanTxNode::amk_status_callback(const putm_vcl_interfaces::msg::AmkStatus msg)
+{
+  if (msg.amk_status_binverter_on[Inverters::FRONT_LEFT] & msg.amk_status_binverter_on[Inverters::FRONT_RIGHT] & msg.amk_status_binverter_on[Inverters::REAR_LEFT] & msg.amk_status_binverter_on[Inverters::REAR_RIGHT])
+  {
+    pcMainData.inverters_ready = true;
+  }
+  else
+  {
+    pcMainData.inverters_ready = false;
+  }
+
 }
 
 void CanTxNode::rtd_callback(const putm_vcl_interfaces::msg::Rtd msg)
