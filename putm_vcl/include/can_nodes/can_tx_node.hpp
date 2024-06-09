468ba394c216788c0eb0740516d355bd916c545d
Adam <Adam_Wasilewski_99@proton.me>
2024-06-09 16:07:13 +0200

    Add pc frame and rtd subscription

--- putm_vcl/include/can_nodes/can_tx_node.hpp
+++ putm_vcl/include/can_nodes/can_tx_node.hpp
@@ -2,6 +2,7 @@
 
 #include "PUTM_DV_CAN_LIBRARY_RAII_2024/include/can_tx.hpp"
 #include "putm_vcl_interfaces/msg/amk_control.hpp"
+#include "putm_vcl_interfaces/msg/rtd.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 class CanTxNode : public rclcpp::Node {
@@ -17,13 +18,17 @@ class CanTxNode : public rclcpp::Node {
   PUTM_CAN::AmkRearLeftSetpoints1 amk_rear_left_setpoints;
   PUTM_CAN::AmkRearRightSetpoints1 amk_rear_right_setpoints;
 
+  PUTM_CAN::PcMainData pcMainData;
+
   rclcpp::TimerBase::SharedPtr can_tx_amk_timer;
   rclcpp::TimerBase::SharedPtr can_tx_common_timer;
 
   rclcpp::Subscription<putm_vcl_interfaces::msg::AmkControl>::SharedPtr amk_control_subscription;
+  rclcpp::Subscription<putm_vcl_interfaces::msg::Rtd>::SharedPtr rtd_subscription;
 
   void can_tx_amk_callback();
   void can_tx_common_callback();
 
   void amk_control_callback(const putm_vcl_interfaces::msg::AmkControl msg);
+  void rtd_callback (const putm_vcl_interfaces::msg::Rtd msg);
 };
